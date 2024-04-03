20ebfaa58bffaeb08decdc9abbd083a7e632143e
Paulo Bastos <paulosetembro10@hotmail.com>
2024-04-03 16:01:33 +0100

    Added autoindex function

--- src/http_handler/http_handler.cpp
+++ src/http_handler/http_handler.cpp
@@ -1,15 +1,19 @@
 #include "http_handler.hpp"
 #include "webserver.hpp"
+#include <dirent.h>
 #include <fstream>
 #include <iostream>
 #include <sys/socket.h>
 #include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 HttpHandler::HttpHandler(const std::string &request, Server &server)
     : request_(request), server_(&server) {}
 
 HttpHandler::~HttpHandler() {}
+
+#pragma region HTTP Methods
 
 /// @brief Process the request
 const std::string HttpHandler::process_request() {
@@ -23,6 +27,79 @@ const std::string HttpHandler::process_request() {
     return get_error_page(501);
   return "";
 }
+
+/// @brief Process a GET request
+const std::string HttpHandler::process_get() {
+  std::fstream file;
+  std::string content;
+  std::string file_path;
+  Stat buffer;
+  std::stringstream ss;
+
+  try {
+    if (request_.get_uri() == "/") {
+      for (size_t i = 0; i < server_->get_config().get_index().size(); i++) {
+        file_path = server_->get_config().get_root() + "/" +
+                    server_->get_config().get_index().at(i);
+        if (stat(file_path.c_str(), &buffer) == 0)
+          break;
+      }
+    } else
+      file_path = server_->get_config().get_root() + "/" + request_.get_uri();
+    if (*(request_.get_uri().end() - 1) == '/' &&
+        server_->get_config().get_auto_index()) {
+      file_path = server_->get_config().get_root() + request_.get_uri();
+      content = create_autoindex(file_path, request_.get_uri());
+      if (content.empty())
+        return get_error_page(404);
+      ss << content.length();
+      std::string response =
+          response_builder("200", "OK", "text/html", ss.str());
+      WebServer::log(std::string(HTTP_200) + request_.get_uri(), info);
+      return response + content;
+    }
+    content = read_file(file_path);
+    ss << content.length();
+    std::string response =
+        response_builder("200", "OK", get_content_type(file_path), ss.str());
+    WebServer::log(std::string(HTTP_200) + request_.get_uri(), info);
+    return response + content;
+  } catch (const std::runtime_error &e) {
+    return get_error_page(404);
+  }
+}
+
+/// @brief Process a POST request
+const std::string HttpHandler::process_post() {
+  size_t max_size =
+      get_max_size(server_->get_config().get_max_client_body_size());
+  std::cout << request_.get_request() << std::endl;
+  if (request_.get_body().size() > max_size)
+    return get_error_page(413);
+  std::string response = "Received" + request_.get_body();
+  std::stringstream ss;
+  ss << response.length();
+  return response_builder("201", "Created", "text/plain", ss.str()) + response;
+}
+
+/// @brief Process a DELETE request
+const std::string HttpHandler::process_delete() {
+  std::string file_path;
+  Stat buffer;
+
+  file_path = server_->get_config().get_root() + request_.get_uri();
+  if (stat(file_path.c_str(), &buffer) != 0)
+    return get_error_page(404);
+  if (!(buffer.st_mode & S_IWOTH))
+    return get_error_page(403);
+  if (std::remove(file_path.c_str()))
+    return get_error_page(403);
+  return response_builder("204", "No Content", "text/plain", "0");
+}
+
+#pragma endregion
+
+#pragma region Utility Functions
 
 /// @brief Get the error page
 /// @param status_code The status code
@@ -73,33 +150,39 @@ std::string HttpHandler::get_error_page(const int status_code) {
   }
 }
 
-/// @brief Process a GET request
-const std::string HttpHandler::process_get() {
-  std::fstream file;
-  std::string content;
-  std::string file_path;
-  Stat buffer;
-  std::stringstream ss;
-
-  try {
-    if (request_.get_uri() == "/") {
-      for (size_t i = 0; i < server_->get_config().get_index().size(); i++) {
-        file_path = server_->get_config().get_root() + "/" +
-                    server_->get_config().get_index().at(i);
-        if (stat(file_path.c_str(), &buffer) == 0)
-          break;
-      }
-    } else
-      file_path = server_->get_config().get_root() + "/" + request_.get_uri();
-    content = read_file(file_path);
-    ss << content.length();
-    std::string response =
-        response_builder("200", "OK", get_content_type(file_path), ss.str());
-    return response + content;
-    WebServer::log(std::string(HTTP_200) + request_.get_uri(), info);
-  } catch (const std::runtime_error &e) {
-    return get_error_page(404);
+/// @brief Create an autoindex page
+/// @param path The path of the directory
+/// @param uri The URI of the directory
+/// @return The autoindex page content
+const std::string HttpHandler::create_autoindex(const std::string &path,
+                                                const std::string &uri) {
+  std::string autoindex;
+  autoindex = "<!DOCTYPE html>\n<html>\n<head>\n<title>Index of " + uri +
+              "</title>\n<link rel=\"stylesheet\" href=\"styles.css\"> "
+              "\n</head>\n<body>\n<h1 id=\"autoindex\">Index of " +
+              uri + "</h1>\n";
+  autoindex += "<table>\n<tr>\n<th>Name</th>\n<th>Last Modified</th>\n";
+  autoindex += "<th>Size</th>\n</tr>\n";
+  DIR *dir;
+  struct dirent *ent;
+  if ((dir = opendir(path.c_str())) != NULL) {
+    while ((ent = readdir(dir)) != NULL) {
+      autoindex += "<tr>\n<td><a href=\"" + std::string(ent->d_name) + "\">" +
+                   std::string(ent->d_name) + "</a></td>\n";
+      struct stat st;
+      std::stringstream ss;
+      std::string file_path = path + "/" + std::string(ent->d_name);
+      stat(file_path.c_str(), &st);
+      ss << st.st_size;
+      autoindex += "<td>" + std::string(ctime(&st.st_mtime)) + "</td>\n";
+      autoindex += "<td>" + ss.str() + " B</td>\n</tr>\n";
+    }
+    closedir(dir);
+  } else {
+    return "";
   }
+  autoindex += "</table>\n</body>\n</html>";
+  return autoindex;
 }
 
 /// @brief Get the maximum size of the client body
@@ -128,34 +211,6 @@ size_t HttpHandler::get_max_size(const std::string &max_size) {
     break;
   }
   return size;
-}
-
-/// @brief Process a POST request
-const std::string HttpHandler::process_post() {
-  size_t max_size =
-      get_max_size(server_->get_config().get_max_client_body_size());
-  std::cout << request_.get_request() << std::endl;
-  if (request_.get_body().size() > max_size)
-    return get_error_page(413);
-  std::string response = "Received" + request_.get_body();
-  std::stringstream ss;
-  ss << response.length();
-  return response_builder("201", "Created", "text/plain", ss.str()) + response;
-}
-
-/// @brief Process a DELETE request
-const std::string HttpHandler::process_delete() {
-  std::string file_path;
-  Stat buffer;
-
-  file_path = server_->get_config().get_root() + request_.get_uri();
-  if (stat(file_path.c_str(), &buffer) != 0)
-    return get_error_page(404);
-  if (!(buffer.st_mode & S_IWOTH))
-    return get_error_page(403);
-  if (std::remove(file_path.c_str()))
-    return get_error_page(403);
-  return response_builder("204", "No Content", "text/plain", "0");
 }
 
 /// @brief Build the response header
@@ -239,3 +294,5 @@ std::string HttpHandler::get_content_type(const std::string &file_path) {
     return ("application/x-www-form-urlencoded");
   return ("text/plain");
 }
+
+#pragma endregion
--- src/http_handler/http_handler.hpp
+++ src/http_handler/http_handler.hpp
@@ -35,6 +35,8 @@ class HttpHandler {
 
     std::string get_error_page(const int status_code);
 
+    const std::string create_autoindex(const std::string &path, const std::string &uri);
+
     bool is_text_file(const std::string &file_path);
 
     size_t get_max_size(const std::string &max_size);
